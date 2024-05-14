255225dc3fff604c5f69cdb87f968cc24b2b08f2
Stanislav-Mac <stanislav_2010@icloud.com>
2024-05-14 16:44:41 +0500

    Start

--- dist/JS/bundle.js
+++ dist/JS/bundle.js
@@ -1,1 +1,32 @@
-document.addEventListener("DOMContentLoaded",(function(){}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/***/ (() => {
+
+eval("document.addEventListener('DOMContentLoaded', function () {\n    if (window.innerWidth <= 768) {\n        const burger = document.querySelector(\".header__btn-burger\");\n        const menu = document.querySelector(\".header__menu\");\n        const header = document.querySelector(\".header\");\n\n        document.body.append(menu);\n        menu.style.paddingTop = header.clientHeight + \"px\";\n\n        burger.addEventListener(\"click\", () => {\n            burger.classList.toggle(\"active\");\n            menu.classList.toggle(\"active\");\n            document.body.classList.toggle(\"lock\");\n        });\n    }\n\n    if (window.innerWidth <= 576) {\n        if (document.querySelector(\"[src-mob]\")) {\n            document.querySelectorAll(\"[src-mob]\").forEach(img => {\n                img.src = img.getAttribute(\"src-mob\");\n            })\n        }\n    }\n});\n\n//# sourceURL=webpack://project-name/./src/js/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/index.js"]();
+/******/ 	
+/******/ })()
+;
--- src/JS/index.js
+++ src/JS/index.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // const burger = document.querySelector(".header__burger");
-    // const menu = document.querySelector(".header__col--menu");
-    // const header = document.querySelector("header");
+    if (window.innerWidth <= 768) {
+        const burger = document.querySelector(".header__btn-burger");
+        const menu = document.querySelector(".header__menu");
+        const header = document.querySelector(".header");
 
-    // burger.addEventListener("click", () => {
-    //     burger.classList.toggle("active");
-    //     menu.classList.toggle("active");
-    //     document.body.classList.toggle("lock");
-    // });
+        document.body.append(menu);
+        menu.style.paddingTop = header.clientHeight + "px";
+
+        burger.addEventListener("click", () => {
+            burger.classList.toggle("active");
+            menu.classList.toggle("active");
+            document.body.classList.toggle("lock");
+        });
+    }
+
+    if (window.innerWidth <= 576) {
+        if (document.querySelector("[src-mob]")) {
+            document.querySelectorAll("[src-mob]").forEach(img => {
+                img.src = img.getAttribute("src-mob");
+            })
+        }
+    }
 });
